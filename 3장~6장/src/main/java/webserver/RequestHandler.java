03863b8e5f14eafabcf23f2b206c8f7a80b25fe2
jikimee64 <jikimee64@gmail.com>
2020-02-06 11:27:51 +0900

    3장 요구사항2 - GET방식으로 회원가입

--- 3장~6장/src/main/java/webserver/RequestHandler.java
+++ 3장~6장/src/main/java/webserver/RequestHandler.java
@@ -2,10 +2,17 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.URLDecoder;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -26,7 +33,22 @@ public class RequestHandler extends Thread {
             String header = br.readLine();
             String[] tokens = header.split(" ");
             String url = tokens[1];
-
+            int index = url.indexOf("?");
+            String requestPath = url.substring(0, index);
+            String params = url.substring(index+1);
+            List<String> a = new ArrayList();
+            Map<String, String> map = HttpRequestUtils.parseQueryString(params);
+            Set<Map.Entry<String, String>> entries = map.entrySet();
+            for(Map.Entry<String, String> entry : entries){
+                a.add(entry.getValue());
+            }
+            String userID = a.get(0), userPassword = a.get(1), userName = a.get(2), userEmail = a.get(3);
+            String decodeUserEmail = URLDecoder.decode(userEmail, "UTF-8");
+            User user = new User(userID, userPassword, userName, decodeUserEmail);
+//            String userID = temp("userID");
+//            String userPassword = params.substring(c+1, d);
+//            String userName = params.substring(e+1, f);
+//            String userEmail = params.substring(g+1);
 //            while(!"".equals(br.readLine())){
 //                if(br.readLine() == null){
 //                    return;
---
+++ 3장~6장/src/test/java/webserver/RequestHandlerTest.java
@@ -0,0 +1,20 @@
+//package webserver;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//public class RequestHandlerTest {
+//    private RequestHandler handler;
+//
+//    @Before
+//    public void setup(){
+//        handler = new RequestHandler();
+//    }
+//
+//
+//    @Test
+//    public void 요구사항2(){
+//
+//
+//    }
+//}
